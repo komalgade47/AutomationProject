899eef76e059c2b70fb0403d4f8945dfb217b683
komalgade47 <komalgade47@gmail.com>
2025-02-06 18:12:28 +0530

    Third commit

---
+++ src/Lab006_Operatores.java
@@ -0,0 +1,12 @@
+public class Lab006_Operatores {
+    public static void main ( String [] arg )
+    {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+    }
+}
---
+++ src/Lab007_Operators_relational.java
@@ -0,0 +1,20 @@
+public class Lab007_Operators_relational {
+    public static void main ( String[] arg)
+    {
+        int a = 10;
+        int b= 20;
+        boolean c = a > b;
+        System.out.println(c);
+
+        boolean d= a < b;
+        System.out.println(d);
+
+        System.out.println(a>b);
+
+      /* boolean e= a<=b;
+        System.out.println(e);
+
+        boolean f= a>=b;
+        System.out.println(f);*/
+    }
+}
