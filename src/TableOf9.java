e11a74e67dae9521d6c7e97dcb42b3a580c01c3e
komalgade47 <komalgade47@gmail.com>
2025-02-06 12:43:32 +0530

    Third commit

---
+++ src/TableOf9.java
@@ -0,0 +1,26 @@
+public class TableOf9 {
+    public static void main( String [] arg ) {
+        int num = 9;
+        System.out.printf("%d * 1 = 9", num);
+        System.out.println();
+        System.out.printf("%d * 2 = 18", num);
+        System.out.println();
+        System.out.printf("%d * 3 = 27", num);
+        System.out.println();
+        System.out.printf("%d * 4 = 36", num);
+        System.out.println();
+        System.out.printf("%d * 5 = 45", num);
+        System.out.println();
+        System.out.printf("%d * 6 = 54", num);
+        System.out.println();
+        System.out.printf("%d * 7 = 63", num);
+        System.out.println();
+        System.out.printf("%d * 8 = 72", num);
+        System.out.println();
+        System.out.printf("%d * 9 = 81", num);
+        System.out.println();
+        System.out.printf("%d * 10 = 90", num);
+        System.out.println();
+    }
+
+}
