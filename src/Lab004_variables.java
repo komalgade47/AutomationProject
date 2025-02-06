86ca69f91ea1a6eb375e742b0c818193e2f6b45f
komalgade47 <komalgade47@gmail.com>
2025-02-06 17:32:38 +0530

    Third commit

---
+++ src/Lab004_variables.java
@@ -0,0 +1,14 @@
+public class Lab004_variables {
+    public static void main ( String [] arg )
+    {
+        int b= 10;
+        char c1 = 'A';
+        float  f=10.54f;
+        int a1= 'a';
+        System.out.println(b);
+        System.out.println(c1);
+        System.out.println(f);
+        System.out.println(a1);
+
+    }
+}
---
+++ src/Lab005_Literals.java
@@ -0,0 +1,15 @@
+public class Lab005_Literals {
+    public static void main ( String[] arg )
+    {
+//escape characters
+        char new_line = '\n';
+        char tab_line ='\t';
+        char back_space ='\b';
+        char carriage_return='\r';
+
+        System.out.println("Komal" + new_line + "Gade");
+        System.out.println("Komal" + tab_line + "Gade");
+        System.out.println("Komal" + back_space + "Gade");
+        System.out.println("Komal" + carriage_return + "Gade");
+    }
+}
